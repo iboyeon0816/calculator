435701ff97b85cac6fe5249d5e5709fde7e6036c
iboyeon0816 <iboyeon0816@gmail.com>
2025-01-06 18:22:37 +0900

    feat(lv3): OperatorType enum에 연산 로직 추가

--- src/main/java/com/example/lv3/app/enums/OperatorType.java
+++ src/main/java/com/example/lv3/app/enums/OperatorType.java
@@ -2,19 +2,29 @@ package com.example.lv3.app.enums;
 
 import java.util.Arrays;
 import java.util.Optional;
+import java.util.function.BiFunction;
 
 public enum OperatorType {
-    ADD('+'), SUBTRACT('-'), MULTIPLY('*'), DIVIDE('/');
+    ADD('+', Double::sum),
+    SUBTRACT('-', (num1, num2) -> num1 - num2),
+    MULTIPLY('*', (num1, num2) -> num1 * num2),
+    DIVIDE('/', (num1, num2) -> num1 / num2);
 
     private final char symbol;
+    private final BiFunction<Double, Double, Double> expression;
 
-    OperatorType(char symbol) {
+    OperatorType(char symbol, BiFunction<Double, Double, Double> expression) {
         this.symbol = symbol;
+        this.expression = expression;
     }
 
     public static Optional<OperatorType> findBySymbol(char symbol) {
         return Arrays.stream(values())
                 .filter(o -> o.symbol == symbol)
                 .findFirst();
+    }
+
+    public double evaluate(double num1, double num2) {
+        return this.expression.apply(num1, num2);
     }
 }
